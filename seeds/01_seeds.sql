c6f3548a6ead5119992f23e39be85e0325a3bb34
mariannebourcier <marianne.bourcier@gmail.com>
2022-06-09 21:30:23 -0600

    Add data to users table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Brandon Gervais', 'bdg@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Marianne Bourcier', 'mb@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+    INSERT INTO users (
+    name, email, password) 
+    VALUES (
+    'Nolan Gervais', 'dog@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');

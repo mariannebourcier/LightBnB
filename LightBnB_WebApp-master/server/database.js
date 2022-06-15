92f86c7c6ed226889b802fee2d5d627bd0517ba4
mariannebourcier <marianne.bourcier@gmail.com>
2022-06-15 12:47:58 -0600

    Refactor addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -159,9 +159,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  const propQuery = (`INSERT INTO properties
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES 
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, TRUE)
+  RETURNING *;`);
+
+  let propObj = [];
+  //loop thru all properties
+  for (let prop in property) {
+    propObj = property[prop];
+  }
+
+  return pool
+    .query(
+      propQuery,
+      propObj
+    )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      return err.message;
+    });
 };
 exports.addProperty = addProperty;
